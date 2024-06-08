b1aafc9fa8754d199340a910d2baed2ff6eb66c0
Ademola <gbengaadepoju75@gmail.com>
2024-06-08 04:03:10 -0500

    loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
 import Loader from "./components/loader";
+import Try from "./components/try";
 
 export default function Home() {
   return (
     <main className="bg-black">
       <Loader/>
       <h1 className='text-red-500 text-2xl grate'>PLAYERS</h1>
-      <Image
-      src="./assets/sporting-logo_biealj.svg"
-      alt="logp"
-      width={300}
-      height={300}/>
+     <Try/>
     </main>
   );
 }
