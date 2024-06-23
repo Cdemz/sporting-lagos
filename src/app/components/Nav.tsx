98bd28f66acaed0ebd0a9f9c04142ea198915847
Ademola <gbengaadepoju75@gmail.com>
2024-06-23 05:18:46 -0500

    font m0dal

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,11 +1,11 @@
-"use client"
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState, useEffect, useRef } from 'react';
-import { BiLogoInstagramAlt } from 'react-icons/bi';
-import { FaFacebook, FaXTwitter } from 'react-icons/fa6';
-import { FiSearch, FiMenu } from 'react-icons/fi';
-import { MdClose } from 'react-icons/md';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { FaFacebook, FaXTwitter } from "react-icons/fa6";
+import { FiSearch, FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,10 +21,10 @@ const Navbar: React.FC = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -38,12 +38,23 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className={`fixed top-0 left-0 w-full  transition-all duration-300 h-14
-      ${isMenuOpen ? 'bg-[var(--color-primary)] ' : isScrolled ? 'bg-[var(--color-primary)]' : 'bg-transparent'}`}>
+    <nav
+      className={`fixed top-0 left-0 w-full  transition-all duration-300 h-14
+      ${
+        isMenuOpen
+          ? "bg-[var(--color-primary)] "
+          : isScrolled
+          ? "bg-[var(--color-primary)]"
+          : "bg-transparent"
+      }`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white focus:outline-none">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-white focus:outline-none"
+            >
               {isMenuOpen ? <MdClose size={24} /> : <FiMenu size={24} />}
             </button>
           </div>
@@ -51,22 +62,27 @@ const Navbar: React.FC = () => {
             <Link href="/">
               <Image
                 src="/sp_logo.png"
-                alt='logo'
+                alt="logo"
                 width={300}
                 height={300}
-                className='h-9 w-6'
+                className="h-9 w-6"
               />
             </Link>
           </div>
           <div className="flex items-center">
-            <button onClick={handleSearchClick} className="text-white focus:outline-none">
+            <button
+              onClick={handleSearchClick}
+              className="text-white focus:outline-none"
+            >
               <FiSearch size={24} />
             </button>
           </div>
         </div>
       </div>
-      <div className={` fixed justify-between top-[3.4rem] z-50 left-0 w-full h-[100vh] bg-[var(--color-primary)] text-white transform transition-transform duration-300
-        ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+      <div
+        className={` fixed justify-between top-[3.4rem] z-50 left-0 w-full h-[100vh] bg-[var(--color-primary)] text-white transform transition-transform duration-300
+        ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}
+      >
         <div className="px-4 pt-6 pb-8 capitalize h-full">
           <ul className="space-y-2 grate capitalize text-2xl font-extrabold">
             <li className="px-2 py-1 capitalize">NEWS</li>
@@ -76,7 +92,7 @@ const Navbar: React.FC = () => {
             <li className="px-2 py-1">MEMBERSHIP</li>
             <li className="px-2 py-1">SPORTING TV</li>
           </ul>
-          
+
           <div className="mt-32 relative">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <FiSearch className="text-gray-400" size={24} />
@@ -89,11 +105,23 @@ const Navbar: React.FC = () => {
             />
           </div>
           <div className="grate mt-10">
-            <p className='text-md'>FOLLOW SPORTING</p>
-            <ul className='flex gap-3'>
-              <Link href="/"><BiLogoInstagramAlt size={27} /></Link>
-              <Link href="/"><FaXTwitter size={25} /></Link>
-              <Link href="/"><FaFacebook size={25}/></Link>
+            <p className="text-md">FOLLOW SPORTING</p>
+            <ul className="flex gap-3">
+              <Link
+                href="https://www.instagram.com/sportinglagos?igsh=MTNmanI4MGlkZmprag=="
+                target="blank"
+              >
+                <BiLogoInstagramAlt size={27} />
+              </Link>
+              <Link href="https://x.com/SportingLagos?s=09" target="blank">
+                <FaXTwitter size={25} />
+              </Link>
+              <Link
+                href="https://www.facebook.com/SportingLagosFC"
+                target="blank"
+              >
+                <FaFacebook size={25} />
+              </Link>
             </ul>
           </div>
         </div>
