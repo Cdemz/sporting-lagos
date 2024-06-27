277c8da64e3daded4fb71be9e2f193e9e61bcab2
Ademola <gbengaadepoju75@gmail.com>
2024-06-27 09:58:14 -0500

    animation on academy page done

--- src/app/academy/welcome.tsx
+++ src/app/academy/welcome.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import Countdown from "./countdown";
-import { motion } from "framer-motion";
+import AOS from "aos";
+import "aos/dist/aos.css";
+// import { motion } from "framer-motion";
 
 const welcome = () => {
   const leftVariants = {
@@ -13,6 +16,14 @@ const welcome = () => {
     hidden: { x: 100, opacity: 0 },
     visible: { x: 0, opacity: 1 },
   };
+
+  useEffect(() => {
+    AOS.init({
+      duration: 700, // Set the duration of the animation
+      once: false, // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
+
   const targetDate = "2023-12-31T00:00:00";
   return (
     <div className="relative w-full h-[70vh] lg:h-[85vh] bg-black">
@@ -25,55 +36,58 @@ const welcome = () => {
       />
 
       <div className="absolute  text-white grate px-4 lg:px-10  pt-28 lg:pt-20  flex flex-col justify- gap-3 ">
-        <motion.div
+        {/* <motion.div
           className="bg-red-500 w-1/2 h-64"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: false }}
           transition={{ duration: 1 }}
           variants={leftVariants}
-        >
-          <div className=" ">
-            <h2 className=" uppercase text-[var(--color-primary)] font-extrabold">
-              Welcome to{" "}
-            </h2>
-            <h1 className="uppercase text-xl font-extrabold mb-3 lg:text-2xl ">
-              Sporting Lagos Academy
-            </h1>
-            <p>Learn from the best coaches in the world</p>
-            <div className="flex flex-col gap-3">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
-                atque nesciunt.
-              </p>
-              <p className="mt">
-                Iure est, culpa itaque blanditiis ea fugiat accusamus! Eius fuga
-                eum vitae nobis, dolores ad natus reprehenderit officia!
-              </p>{" "}
-            </div>
-            <button className="bg-blue-600 py-4 px-6 rounded-3xl rounded-s-xls mt-5 mb-2">
-              Read More
-            </button>
+        > */}
+        <div className=" " data-aos="fade-right">
+          <h2 className=" uppercase text-[var(--color-primary)] font-extrabold">
+            Welcome to{" "}
+          </h2>
+          <h1 className="uppercase text-xl font-extrabold mb-3 lg:text-2xl ">
+            Sporting Lagos Academy
+          </h1>
+          <p>Learn from the best coaches in the world</p>
+          <div className="flex flex-col gap-3">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex atque
+              nesciunt.
+            </p>
+            <p className="mt">
+              Iure est, culpa itaque blanditiis ea fugiat accusamus! Eius fuga
+              eum vitae nobis, dolores ad natus reprehenderit officia!
+            </p>{" "}
           </div>
-        </motion.div>
-        <motion.div
+          <button className="bg-blue-600 py-4 px-6 rounded-3xl rounded-s-xls mt-5 mb-2">
+            Read More
+          </button>
+        </div>
+        {/* </motion.div> */}
+        {/* <motion.div
           className="bg-red-500 w-1/2 h-64"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: false }}
           transition={{ duration: 1 }}
           variants={rightVariants}
+        > */}
+        <div
+          data-aos="fade-left"
+          className="text-center py-3 px-6 bg-[var(--color-primary)] grate lg:w-[40%] "
         >
-          <div className="text-center py-3 px-6 bg-[var(--color-primary)] grate lg:w-[40%] ">
-            <h1 className="text-2xl font-bold  uppercase text-blue-600">
-              Next Match
-            </h1>
-            <h3 className="text-xl font-semibold uppercase mt-1">
-              Sporting vs Ojodu FC
-            </h3>
-            <Countdown targetDate={targetDate} />
-          </div>
-        </motion.div>
+          <h1 className="text-2xl font-bold  uppercase text-blue-600">
+            Next Match
+          </h1>
+          <h3 className="text-xl font-semibold uppercase mt-1">
+            Sporting vs Ojodu FC
+          </h3>
+          <Countdown targetDate={targetDate} />
+        </div>
+        {/* </motion.div> */}
       </div>
     </div>
   );
